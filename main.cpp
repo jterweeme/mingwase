bbdd86ace1e1ae45291db8f080ea895a0c003b39
Jasper ter Weeme <jterweeme@gmail.com>
2015-06-03 19:54:16 +0200

    initial

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+
+class MainClass
+{
+public:
+    
+};
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+}
+
+int WINAPI WinMain(HINSTANCE h, HINSTANCE p, char *c, int n)
+{
+    return 0;
+}
+
+
